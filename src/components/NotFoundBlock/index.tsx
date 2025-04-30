432d244f4f771de095c00c01f8ab1b4b180b7a9a
SanteriValo <valosanteri@gmail.com>
2025-04-30 16:21:11 +0300

    feat: add NotFound component with SCSS module

---
+++ src/components/NotFoundBlock/index.tsx
@@ -0,0 +1,13 @@
+import styles from "./NotFoundBlock.module.scss";
+
+const NotFoundBlock = () => {
+  return (
+    <h1 className={styles.root}>
+      <span>😔</span>
+      <br />
+      Not found
+    </h1>
+  );
+};
+
+export default NotFoundBlock;
