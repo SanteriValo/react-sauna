6cdf8d6c657ebe79d27b4722dd998f4765e83655
SanteriValo <valosanteri@gmail.com>
2025-04-30 16:20:28 +0300

    feat: add react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,16 @@
 import "./styles/app.scss";
 import Header from "./components/Header.tsx";
-import Categories from "./components/Categories.tsx";
-import Sort from "./components/Sort.tsx";
-import ItemBlock from "./components/ItemBlock/";
-import { useEffect, useState } from "react";
-import Skeleton from "./components/ItemBlock/Skeleton.tsx";
+import Home from "./pages/Home.tsx";
+import NotFound from "./pages/NotFound.tsx";
 
 const App = () => {
-  const [items, setItems] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
-
-  useEffect(() => {
-    fetch("https://680bb9062ea307e081d21a74.mockapi.io/items")
-      .then((res) => {
-        return res.json();
-      })
-      .then((data) => {
-        setItems(data);
-        setIsLoading(false);
-      });
-  }, []);
-
   return (
     <div>
       <div className="wrapper">
-        {Header()}
+        <Header />
         <div className="content">
           <div className="container">
-            <div className="content__top">
-              {Categories()}
-              {Sort()}
-            </div>
-            <h2 className="content__title">All items</h2>
-            <div className="content__items">
-              {isLoading
-                ? [...new Array(6)].map((_, index) => <Skeleton key={index} />)
-                : items.map((obj) => <ItemBlock key={obj.id} {...obj} />)}
-            </div>
+            <Home />
           </div>
         </div>
       </div>
---
+++ src/pages/Home.tsx
@@ -0,0 +1,36 @@
+import Categories from "../components/Categories.tsx";
+import Sort from "../components/Sort.tsx";
+import Skeleton from "../components/ItemBlock/Skeleton.tsx";
+import ItemBlock from "../components/ItemBlock";
+import { useEffect, useState } from "react";
+
+export const Home = () => {
+  const [items, setItems] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    fetch("https://680bb9062ea307e081d21a74.mockapi.io/items")
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        setItems(data);
+        setIsLoading(false);
+      });
+  }, []);
+
+  return (
+    <>
+      <div className="content__top">
+        {Categories()}
+        {Sort()}
+      </div>
+      <h2 className="content__title">All items</h2>
+      <div className="content__items">
+        {isLoading
+          ? [...new Array(6)].map((_, index) => <Skeleton key={index} />)
+          : items.map((obj) => <ItemBlock key={obj.id} {...obj} />)}
+      </div>
+    </>
+  );
+};
