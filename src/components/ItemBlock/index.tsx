78486aeee79c38d0b72984a1666deb0c5aaa392f
SanteriValo <valosanteri@gmail.com>
2025-05-13 17:21:49 +0300

    refactor: add responsive grid to item-blocks

--- src/components/ItemBlock/index.tsx
+++ src/components/ItemBlock/index.tsx
@@ -6,55 +6,57 @@ const Index = ({ name, price, imageUrl, power, color }) => {
   const colors = ["black", "silver"];
 
   return (
-    <div className="item-block">
-      <h4 className="item-block__title">{name}</h4>
-      <img className="item-block__image" src={imageUrl} alt="Harvia Stove" />
-      <div className="item-block__selector">
-        <ul>
-          {color.map((color) => (
-            <li
-              key={color}
-              onClick={() => {
-                setActiveColor(color);
-              }}
-              className={activeColor === color ? "active" : ""}
+    <div className="item-block-wrapper">
+      <div className="item-block">
+        <h4 className="item-block__title">{name}</h4>
+        <img className="item-block__image" src={imageUrl} alt="Harvia Stove" />
+        <div className="item-block__selector">
+          <ul>
+            {color.map((color) => (
+              <li
+                key={color}
+                onClick={() => {
+                  setActiveColor(color);
+                }}
+                className={activeColor === color ? "active" : ""}
+              >
+                {colors[color]}
+              </li>
+            ))}
+          </ul>
+          <ul>
+            {power.map((power, index) => (
+              <li
+                key={power}
+                onClick={() => {
+                  setActivePower(index);
+                }}
+                className={activePower === index ? "active" : ""}
+              >
+                {power} kW
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className="item-block__bottom">
+          <div className="item-block__price">from {price}€</div>
+          <button className="button button--outline button--add">
+            <svg
+              width="12"
+              height="12"
+              viewBox="0 0 12 12"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
             >
-              {colors[color]}
-            </li>
-          ))}
-        </ul>
-        <ul>
-          {power.map((power, index) => (
-            <li
-              key={power}
-              onClick={() => {
-                setActivePower(index);
-              }}
-              className={activePower === index ? "active" : ""}
-            >
-              {power} kW
-            </li>
-          ))}
-        </ul>
-      </div>
-      <div className="item-block__bottom">
-        <div className="item-block__price">from {price}€</div>
-        <button className="button button--outline button--add">
-          <svg
-            width="12"
-            height="12"
-            viewBox="0 0 12 12"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M10.8 4.8H7.2V1.2C7.2 0.5373 6.6627 0 6 0C5.3373 0 4.8 0.5373 4.8 1.2V4.8H1.2C0.5373 4.8 0 5.3373 0 6C0 6.6627 0.5373 7.2 1.2 7.2H4.8V10.8C4.8 11.4627 5.3373 12 6 12C6.6627 12 7.2 11.4627 7.2 10.8V7.2H10.8C11.4627 7.2 12 6.6627 12 6C12 5.3373 11.4627 4.8 10.8 4.8Z"
-              fill="white"
-            />
-          </svg>
-          <span>Add</span>
-          <i>0</i>
-        </button>
+              <path
+                d="M10.8 4.8H7.2V1.2C7.2 0.5373 6.6627 0 6 0C5.3373 0 4.8 0.5373 4.8 1.2V4.8H1.2C0.5373 4.8 0 5.3373 0 6C0 6.6627 0.5373 7.2 1.2 7.2H4.8V10.8C4.8 11.4627 5.3373 12 6 12C6.6627 12 7.2 11.4627 7.2 10.8V7.2H10.8C11.4627 7.2 12 6.6627 12 6C12 5.3373 11.4627 4.8 10.8 4.8Z"
+                fill="white"
+              />
+            </svg>
+            <span>Add</span>
+            <i>0</i>
+          </button>
+        </div>
       </div>
     </div>
   );
