51c137924acd96ef50902670ffd646fd4fda3066
SanteriValo <valosanteri@gmail.com>
2025-05-22 23:41:02 +0300

    style: add icons

--- src/components/SearchBlock/index.tsx
+++ src/components/SearchBlock/index.tsx
@@ -8,6 +8,7 @@ const SearchBlock = ({ searchValue, setSearchValue }) => {
         <h3>Search:</h3>
         <input
           onChange={(event) => setSearchValue(event.target.value)}
+          value={searchValue}
           className={styles.searchInput}
           placeholder="sauna products..."
         />
@@ -40,6 +41,19 @@ const SearchBlock = ({ searchValue, setSearchValue }) => {
               strokeMiterlimit: 10,
               strokeWidth: 32,
             }}
+          />
+        </svg>
+        <svg
+          className={styles.iconClear}
+          xmlns="http://www.w3.org/2000/svg"
+          xmlSpace="preserve"
+          viewBox="0 0 32 32"
+          width={24}
+          height={24}
+        >
+          <path
+            fill="#121313"
+            d="m17.459 16.014 8.239-8.194a.992.992 0 0 0 0-1.414 1.016 1.016 0 0 0-1.428 0l-8.232 8.187L7.73 6.284a1.009 1.009 0 0 0-1.428 0 1.015 1.015 0 0 0 0 1.432l8.302 8.303-8.332 8.286a.994.994 0 0 0 0 1.414 1.016 1.016 0 0 0 1.428 0l8.325-8.279 8.275 8.276a1.009 1.009 0 0 0 1.428 0 1.015 1.015 0 0 0 0-1.432l-8.269-8.27z"
           />
         </svg>
       </div>
