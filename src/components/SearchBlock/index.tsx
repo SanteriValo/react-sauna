cb9a613018d0abe7f34cee24d2878bec7ad68fa2
SanteriValo <valosanteri@gmail.com>
2025-05-24 23:20:41 +0300

    style: fix icons in search input

--- src/components/SearchBlock/index.tsx
+++ src/components/SearchBlock/index.tsx
@@ -13,43 +13,12 @@ const SearchBlock = ({ searchValue, setSearchValue }) => {
           placeholder="sauna products..."
         />
         <svg
-          className={styles.icon}
-          height="20"
-          width="20"
-          viewBox="0 0 512 512"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <title>Search Icon</title>
-          <path
-            d="M221.09,64A157.09,157.09,0,1,0,378.18,221.09,157.1,157.1,0,0,0,221.09,64Z"
-            style={{
-              fill: "none",
-              stroke: "#000",
-              strokeMiterlimit: 10,
-              strokeWidth: 32,
-            }}
-          />
-          <line
-            x1="338.29"
-            x2="448"
-            y1="338.29"
-            y2="448"
-            style={{
-              fill: "none",
-              stroke: "#000",
-              strokeLinecap: "round",
-              strokeMiterlimit: 10,
-              strokeWidth: 32,
-            }}
-          />
-        </svg>
-        <svg
           className={styles.iconClear}
           xmlns="http://www.w3.org/2000/svg"
           xmlSpace="preserve"
           viewBox="0 0 32 32"
-          width={24}
-          height={24}
+          width={16}
+          height={16}
         >
           <path
             fill="#121313"
